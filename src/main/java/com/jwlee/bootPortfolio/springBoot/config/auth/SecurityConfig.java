9cc54f5cadc6f8846aaeae1afea95feb08692f6e
j369369 <ljw559@naver.com>
2020-12-20 22:57:10 +0900

    profile 추가 ( 무중단배포를 위한 profile 설정)

--- src/main/java/com/jwlee/bootPortfolio/springBoot/config/auth/SecurityConfig.java
+++ src/main/java/com/jwlee/bootPortfolio/springBoot/config/auth/SecurityConfig.java
@@ -18,7 +18,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
                 .and()
                 .authorizeRequests()    // URL별 권한 관리를 설정하는 옵션의 시작점 -> 선언되어야 antMatchers 를 사용할수있음
                 .antMatchers("/","/css/**","/images/**",    // antMatchers = 권한관리대상을 지정하는 옵션
-                        "/js/**","/h2-console/**").permitAll()  // "/"등 지정된 URL들은 permitAll 옵션을 통해 전체 열람 권한
+                        "/js/**","/h2-console/**","/profile").permitAll()  // "/"등 지정된 URL들은 permitAll 옵션을 통해 전체 열람 권한
                 .antMatchers("/api/v1/**").hasRole(Role.USER.name()) // /api/vi/** 은 USER 권한 가진 사람만 가능하게 옵션
                 .anyRequest().authenticated()   // anyRequest -> 이외 모든 요청들을 authenticated -> 모두 인증된 사용자만 사용가능하게 설정
                 .and()
